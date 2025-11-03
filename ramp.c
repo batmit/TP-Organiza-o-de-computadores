d6d46e7ff639fd5b363dd2870cec3bea20529b49
Mateus Heleno <MATEUSHELENO378@GMAIL.COM>
2025-11-03 17:01:09 -0300

    Mundança na memória ram

--- ramp.c
+++ ramp.c
@@ -9,14 +9,21 @@ RAM memoria;
 RAM* criarRAM(int tam){
 
     RAM *r = malloc(sizeof(RAM));
-    r->mem = calloc(tam, sizeof(int)); 
+    r->mem = malloc(tam, sizeof(int)); 
     r->tamanho = tam;
     return r;
 }
 
 
 RAM* criarRAM_vazia(int tam);
-RAM* criarRAM_aleatoria(int tam);
+RAM* criarRAM_aleatoria(int tam){
+
+    RAM *r = malloc(sizeof(RAM));
+    r->mem = calloc(tam, sizeof(int)); 
+    r->tamanho = tam;
+    return r;
+
+}
 void setDado(int endereco, int conteudo);
 void getDado(int endereco);
 void imprimir(void);
