095ec85bed684b93e77afb92f585329f54cc9e68
Mateus Heleno <MATEUSHELENO378@GMAIL.COM>
2025-11-03 17:10:09 -0300

    Mundaça na memoria ram 2

--- ramp.c
+++ ramp.c
@@ -1,25 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 #include "Instrucao.h"
 #include "ram.h"
 
 RAM memoria;
 
+
+
 RAM* criarRAM(int tam){
 
     RAM *r = malloc(sizeof(RAM));
-    r->mem = malloc(tam, sizeof(int)); 
+    r->mem = malloc(tam * sizeof(int)); // cria memoria com lixo
     r->tamanho = tam;
     return r;
+
 }
 
 
-RAM* criarRAM_vazia(int tam);
-RAM* criarRAM_aleatoria(int tam){
+RAM* criarRAM_vazia(int tam){
 
     RAM *r = malloc(sizeof(RAM));
-    r->mem = calloc(tam, sizeof(int)); 
+    r->mem = calloc(tam, sizeof(int));  // cria memoria com 0 (vazia)
+    r->tamanho = tam;
+    return r;
+
+}
+
+RAM* criarRAM_aleatoria(int tam){
+    srand(time(NULL));
+    RAM *r = malloc(sizeof(RAM));
+    r->mem = malloc(tam * sizeof(int));
+    for(int i=0; i<tam; i++)
+			r->mem[i] =rand(); // cria memoria com endereco aleatorio
     r->tamanho = tam;
     return r;
 
